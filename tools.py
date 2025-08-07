6f09ebbee885084fbbb0deecc1d1c7c2dfcef7a5
ali-hjq <ali-hjq@gmail.com>
2025-08-07 12:23:36 +0330

    Refactor tools.py to include shop link activity check and improve link extraction functions

--- tools.py
+++ tools.py
@@ -1,5 +1,8 @@
-from firecrawl import FirecrawlApp
 import re
+import requests
+from bs4 import BeautifulSoup
+
+
 
 
 def is_product_link(url):
@@ -9,3 +12,14 @@ def is_product_link(url):
 def extract_shop_link(product_url):
     shop_link = re.sub(r'/product/\d+', '', product_url)
     return shop_link
+
+def is_shop_active(url):
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    inactive = soup.select_one(
+        "div.bs-container div.oLb_LX img.MAsCzs[alt='این غرفه فعلا غیرفعاله']"
+    )
+    return inactive is None
